e464431b82308e5f07d858500ca2a83f2ca960d5
Cinnamon18 <>
2026-04-17 23:21:42 -0700

    restart effect

---
+++ Assets/Scripts/SceneTransitioner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour {
+	private AsyncOperation loadingOp;
+
+	void Start() {
+	}
+
+	public void TransitionSceneTo(string targetScene) {
+		loadingOp = SceneManager.LoadSceneAsync(targetScene);
+		loadingOp.allowSceneActivation = false;
+
+		StartCoroutine(WaitForNextSceneLoaded());
+	}
+
+	private IEnumerator WaitForNextSceneLoaded() {
+		//Just to show off the effect 😇
+		yield return new WaitForSeconds(1);
+
+		// 0.9 is considered loaded for some reason 😅 adjust in kind.
+		while (loadingOp.progress < 0.89) {
+			yield return null;
+		}
+
+		// transiton out
+		// yield return sceneTransAnimator.StopTransitionAnimation();
+
+		loadingOp.allowSceneActivation = true;
+	}
+}

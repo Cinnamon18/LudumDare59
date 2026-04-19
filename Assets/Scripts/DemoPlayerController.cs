35c8fc4fb1c952ec196a0885cf0ea419aa3227da
Cinnamon18 <>
2026-04-18 17:59:50 -0700

    art imported

--- Assets/Scripts/DemoPlayerController.cs
+++ Assets/Scripts/DemoPlayerController.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using Utilities;
 
 public class DemoPlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+	[SerializeField] private Rigidbody2D rb;
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxis("Horizontal") > 0) {
+			rb.linearVelocity += (transform.right.V2() * Time.deltaTime * 15);
+		}
+        if(Input.GetAxis("Vertical") > 0) {
+			rb.linearVelocity += (transform.up.V2() * Time.deltaTime * 15);
+		}
     }
 }

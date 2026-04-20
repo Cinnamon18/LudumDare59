d33572d349680e805120b323ff827536c528be35
Cinnamon18 <>
2026-04-19 21:16:04 -0700

    splat noise

--- Assets/Scripts/Interactables/Killable.cs
+++ Assets/Scripts/Interactables/Killable.cs
@@ -3,7 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Killable : MonoBehaviour {
 	public const string DeathSceneName = "DeathScene";
-	
+	[SerializeField] private AudioSource audioSource;
+
+	void Reset() {
+		audioSource = GetComponentInParent<AudioSource>(true);
+	}
+
 	void OnCollisionEnter2D(Collision2D other) {
 		if(((1 << other.gameObject.layer) & LayerMask.GetMask("Hazard")) != 0) {
 			Debug.Log("Death draws near.");
@@ -11,6 +16,8 @@ public class Killable : MonoBehaviour {
 			DeathScreen.SceneToReturnTo = SceneManager.GetActiveScene().name;
 			var sceneTransitioner = FindObjectsByType<SceneTransitioner>(FindObjectsSortMode.None)[0];
 			sceneTransitioner.TransitionSceneTo(DeathSceneName);
+
+			audioSource.Play();
 		}
 	}
 }
